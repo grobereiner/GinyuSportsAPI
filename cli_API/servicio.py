25903511fc877072e7d15d1c1861a6b3fa0a6cdd
Prober <ugartegrover@gmail.com>
2022-05-20 18:44:44 -0500

    reconexiones

--- cli_API/servicio.py
+++ cli_API/servicio.py
@@ -3,12 +3,19 @@ import controlador
 
 resp = None
 errorNoHost = "EN ESTE MOMENTO NO HAY SERVICIO TOTAL"
-try:
-    resp = requests.get(url="http://127.0.0.1:5000/")
-except:
-    print(errorNoHost)
-    exit(0)
+terminar = 1
+while terminar == 1:
+    try:
+        resp = requests.get(url="http://127.0.0.1:5000/")
+        break
+    except:
+        print(errorNoHost)
+        terminar = int(input("INGRESE 1 PARA RECONECTAR: "))
+        if terminar != 1:
+            exit(0)
 
-controlador.busqueda()
-
-controlador.insercion()
+terminar = 0
+while terminar != 1:
+    controlador.busqueda()
+    controlador.insercion()
+    terminar = int(input("SI YA NO QUIERE REPETIR, PRESIONE 1"))
