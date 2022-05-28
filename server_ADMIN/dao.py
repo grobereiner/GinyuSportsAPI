f17f88fbfe334bd2f341e15b742de783fc51b0bd
GU-U7 <grover.ugarte@utec.edu.pe>
2022-05-27 20:53:12 -0500

    para el testeo

--- server_ADMIN/dao.py
+++ server_ADMIN/dao.py
@@ -1,7 +1,7 @@
-class Resultados(db.Model):
+""" class Resultados(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db)
     team_one = db.Column(db.String(50))
     team_two = db.Column(db.String(50))
     resultado = db.Column(db.String(50))
-    pass
+    pass """
