6bdbe8719b761325435049ad06b71dc713f9d116
GU-U7 <grover.ugarte@utec.edu.pe>
2022-06-03 17:03:18 -0500

    funcionamiento bmq completo

--- server_BMQ/bmq.py
+++ server_BMQ/bmq.py
@@ -1,25 +1,29 @@
 from flask import Flask, request, send_file
+import bmqueue
 
 cache_file = "cache.csv"
 
 app = Flask(__name__)
 
+bmq = bmqueue.BMQueue()
+
 @app.route("/recibir", methods = ['POST'])
 def recibir():
     result = str(request.get_data(cache=False, as_text=True))
-    with open(cache_file, "a") as f:
-        f.write(result)
-        f.close()
+    # with open(cache_file, "a") as f:
+    #     f.write(result)
+    #     f.close()
+    bmq.enqueue(result)
     return ""
 
 @app.route("/enviar")
 def enviar():
-    texto = None
-    with open(cache_file, "r") as f:
-        texto = f.read()
-        f.close()
-    
-    open(cache_file, "w").close()
+    # texto = None
+    # with open(cache_file, "r") as f:
+    #     texto = f.read()
+    #     f.close()
+    # open(cache_file, "w").close()
+    texto = bmq.dequeue()
     return texto
 
 
--- server_UPDATE/servicio.py
+++ server_UPDATE/servicio.py
@@ -2,15 +2,6 @@ import requests
 import psycopg2
 from time import sleep
 
-def get_conn():
-    conn = psycopg2.connect(
-    host="localhost",
-    database="ginyu",
-    user="Grove",
-    password="root")
-    return conn
-
-
 bmq_addr = "http://127.0.0.1:5009/enviar"
 # CAMBIAR PARA UN DIRECTORIO MEJOR
 temp_file_path = "C:/Users/Public/Documents/TEMP/temp.csv"
@@ -20,25 +11,33 @@ queryUpdate += temp_file_path
 queryUpdate += "' DELIMITER ',' CSV HEADER;"
 
 while True:
-    conn = get_conn()
+    conn = psycopg2.connect(
+        host="localhost",
+        database="ginyu",
+        user="Grove",
+        password="root")
     cur = conn.cursor()
 
-    r = requests.get(url=bmq_addr)
-    if len(r.text) == 0:
-        print("No hay recibos en la cola")
-    else:
-        print(r.text)
-        with open(temp_file_path, "w") as f:
-            f.write(r.text)
-            f.close()
-        try:
-            cur.execute(query=queryUpdate)
-            conn.commit()
-            pass
-        except:
-            print("Fallo en la subida")
-        
+    try:
+        r = requests.get(url=bmq_addr)
+    
+        if len(r.text) == 0:
+            print("No hay recibos en la cola")
+        else:
+            print(r.text)
+            with open(temp_file_path, "w") as f:
+                f.write(r.text)
+                f.close()
+            try:
+                cur.execute(query=queryUpdate)
+                conn.commit()
+                pass
+            except:
+                print("Fallo en la subida")
 
+    except:
+        print("NO HAY CONEXION AL BMQ")
+    
     cur.close()
     conn.close()
     sleep(5)
