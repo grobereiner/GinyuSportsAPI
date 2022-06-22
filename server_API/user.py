c84f80c6c190838b020283e61d4e81b927cfe4a6
edu-arr99 <eduardo.arrospide@utec.edu.pe>
2022-06-22 02:08:47 -0500

    arreglo para que no nos hagan ddos xd

--- server_API/user.py
+++ server_API/user.py
@@ -1,9 +1,9 @@
-from flask import session
 from passlib.hash import pbkdf2_sha256
 import pymongo
 import jwt
 import datetime
 from Gmail.gmail import send_token_email
+import re
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.login
@@ -11,25 +11,32 @@ db = client.login
 
 class User:
     def signup(self, email, password, secret_key):
-        # Create the user object
-        user = {
-            "email": email,
-            "password": password
-        }
+        email_regex = "^[a-zA-Z0-9-_.]+@[a-zA-Z0-9]+(\.[a-z]{1,3})+$"
+        # Revisando que el email sea válido - igual no deberían llegar emails no válidos,
+        # pero si alguien tiene acceso al mismo puerto con otro programa, podría registrar
+        # emails no válidos y generar problemas
+        if re.match(email_regex, email):
+            # Se crea el dict user
+            user = {
+                "email": email,
+                "password": password
+            }
 
-    # Encrypt the password
-        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+            # Enciptar password
+            user['password'] = pbkdf2_sha256.encrypt(user['password'])
 
-    # Check for existing email address
-        if db.users.find_one({"email": user['email']}):
-            return "Email already exists"
+            # Check si email ya existe
+            if db.users.find_one({"email": user['email']}):
+                return "Email already exists"
 
-        if db.users.insert_one(user):
-            token = jwt.encode({"user": user["email"], "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, secret_key)
-            send_token_email(token, user["email"])
-            return "Signup success, logged in"
+            if db.users.insert_one(user):
+                token = jwt.encode({"user": user["email"], "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, secret_key)
+                send_token_email(token, user["email"])
+                return "Signup success, logged in"
 
-        return "Signup failed"
+            return "Signup failed"
+            
+        return "Email inválido"
 
     def signout(self):
         return "Signed out"
