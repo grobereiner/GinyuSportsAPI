5aa6ebb60d334b79ea85decd55a02c92dd5a9e90
IanCortez <iancortezg@hotmail.com>
2022-06-24 20:05:33 -0500

    updates

--- swagger_ui/swagger.py
+++ swagger_ui/swagger.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 from flask_swagger_ui import get_swaggerui_blueprint
-from requests import request
+import requests
 
 
 app = Flask(__name__)
@@ -11,13 +11,18 @@ def send_static(path):
 	return send_from_directory('static', path)
 
 
-SWAGGER_URL = '/swagger'
+@app.route("/<query>")
+def search(query):
+	return requests.get(url='http://127.0.0.1:5002/'+query).text
+
+
+SWAGGER_URL = '/docs'
 API_URL = '/static/swagger.json'
 swagger_ui_blueprint = get_swaggerui_blueprint(
 	SWAGGER_URL,
 	API_URL,
 	config={
-		'app_name': 'Ginyu Sports API'
+		'app_name': 'Ginyu Sports API Documentation'
 	}
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
