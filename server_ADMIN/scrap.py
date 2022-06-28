f7d065bb7a906588c404c733dbef2590f588e808
Prober76 <ugartegrover@gmail.com>
2022-06-28 15:33:26 -0500

    refactor

--- server_ADMIN/scrap.py
+++ server_ADMIN/scrap.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 month_number = {'Jan': "01", "Feb": "02", "Mar": "03", "Apr": "04", "May":"05", "Jun": "06","Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+anho = "2022"
 
 def date_format(datestr):
     datestr = datestr.split(' ')
-    anho = "2022"
     mes = month_number[datestr[2]]
     dia = datestr[1].zfill(2)
     return anho+"-"+mes+'-'+dia
@@ -14,7 +14,6 @@ def date_format(datestr):
 def scrapear(params):
     enlace = "https://www.soccerstats.com/results.asp?league="
     mes = "&pmtype=month3"
-    anho = "2022"
 
     # RETRIEVAL OF HTML USING REQUESTS
     html_text = requests.get(enlace+params+mes).text
